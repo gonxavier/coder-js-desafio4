0385e2e03f476e02c8b0bceb7acc5211b0fbaa4c
gonxavier <dominguezgon@gmail.com>
2022-05-31 10:29:18 -0300

    cambios menores

--- assets/js/coberturas.js
+++ assets/js/coberturas.js
@@ -1,6 +1,6 @@
 //BUSQUEDA COBERTURAS EN JSON
-const cardCoberturas = document.getElementById("coberturas");
-fetch('coberturas.json')
+/* const cardCoberturas = document.getElementById("coberturas");
+fetch('https://gonxavier.github.io/coder-js-desafio4/assets/js/coberturas.json')
 .then((datos) => datos.json())
 .then((card) => {
     card.forEach(valor => {
@@ -20,5 +20,25 @@ fetch('coberturas.json')
         div_padre.appendChild(div_hijo1);
         div_padre.appendChild(div_hijo2);
         cardCoberturas.appendChild(div_padre);
+    })
+}) */
+
+const cardCoberturas = document.getElementById("coberturas");
+fetch('https://gonxavier.github.io/coder-js-desafio4/assets/js/coberturas.json')
+.then((datos) => datos.json())
+.then((card) => {
+    card.forEach(valor => {
+        let tarjeta = document.createElement("div");
+        tarjeta.classsName = "card";
+        
+        tarjeta.innerHTML = `<h2 class="card-cobertura">Cobertura (${valor.letra}) ${valor.nombre}</h2>
+        <p class="card-cobertura"><b>Descripción de la cobertura:</b> ${valor.descripcion}</p>
+        <p class="card-cobertura"><b>Responsabilidad Civil:</b> ${valor.rc}</p>
+        <p class="card-cobertura"><b>Robo:</b> ${valor.robo}</p>
+        <p class="card-cobertura"><b>Incendio:</b> ${valor.incendio}</p>
+        <p class="card-cobertura"><b>Daños por accidente:</b> ${valor.accidente}</p>
+        <hr class="card-cobertura">
+        `
+        cardCoberturas.appendChild(tarjeta);
     })
 })
